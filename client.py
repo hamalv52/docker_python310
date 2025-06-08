9f84eda75ed52f3044194c63baad2043c9e3ef8e
hamalv52 <r.kohama0514@gmail.com>
2025-06-08 21:52:27 +0900

    Delete client.py

--- client.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-res = requests.post("http://your-ollama-containername:11434/api/generate", json={
-    "model": "pakachan/elyza-llama3-8b",
-    "prompt": "こんにちは！自己紹介して",
-    "stream": False
-})
-print("💬 モデルからの返答：\n", res.json()["response"])
