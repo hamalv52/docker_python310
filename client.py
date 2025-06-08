a2c5e49321751ad62ec34f68fbcf65984aaecb50
hamalv52 <r.kohama0514@gmail.com>
2025-06-08 20:55:50 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import requests
 
-res = requests.post("http://cnt-ollama-elyza:11434/api/generate", json={
+res = requests.post("http://your-ollama-containername:11434/api/generate", json={
     "model": "pakachan/elyza-llama3-8b",
     "prompt": "こんにちは！自己紹介して",
     "stream": False
